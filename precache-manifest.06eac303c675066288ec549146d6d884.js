669f14759aa32780322809d6e624f5651707756c
Harsh Maisheri <harshmaisheri@gmail.com>
2020-06-08 18:50:58 +0530

    Updates

---
+++ precache-manifest.06eac303c675066288ec549146d6d884.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65772dd47c0af1abd3ebbfc56b521f3",
+    "url": "/Robots-Rolodex/index.html"
+  },
+  {
+    "revision": "2135c45fe7c11321fac9",
+    "url": "/Robots-Rolodex/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "344d00bfcda52f384e38",
+    "url": "/Robots-Rolodex/static/css/main.c194b79f.chunk.css"
+  },
+  {
+    "revision": "2135c45fe7c11321fac9",
+    "url": "/Robots-Rolodex/static/js/2.a8e94af2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Robots-Rolodex/static/js/2.a8e94af2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "344d00bfcda52f384e38",
+    "url": "/Robots-Rolodex/static/js/main.09afd89d.chunk.js"
+  },
+  {
+    "revision": "f63f8e8a751a250903ff",
+    "url": "/Robots-Rolodex/static/js/runtime-main.13d64f92.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots-Rolodex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
