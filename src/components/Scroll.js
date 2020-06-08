555dec1491c1600a07f802454a12c61a9231a19f
Harsh Maisheri <harshmaisheri@gmail.com>
2020-06-08 18:48:27 +0530

    Initial Commit

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,13 +1,8 @@
-import React from 'react';
-
+import React from "react";
 
 const Scroll = (props) => {
-    return(
-        <div style={{position: 'sticky', top: '0', zIndex: '5', padding: '10px', background: 'linear-gradient(to left, rgba(7,27,82,1) 0%, rgba(0,128,128,1) 100%)', border: '1px solid black'}}>
-            {props.children}
-        </div>
-    );
-}
+  return <div className="scroll">{props.children}</div>;
+};
 
 export default Scroll;
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunkMiddleWare from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import './style/index.css';
-import App from './containers/App';
-import 'tachyons';
-import { searchRobots,getRobotsReducer } from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunkMiddleWare from "redux-thunk";
+import { createLogger } from "redux-logger";
+import "./style/index.css";
+import App from "./containers/App";
+import "tachyons";
+import { searchRobots, getRobotsReducer } from "./reducers";
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, getRobotsReducer });
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare, logger));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleWare, logger)
+);
 
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Provider store={store}>
-                    <App/>
-                </Provider>, 
-                document.getElementById('root')
-                );
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
